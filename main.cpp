5b2e357cc18368e6bab44d564eca554ae52d1a29
namgivu <namgivu@gmail.com>
2017-08-30 10:35:35 +0700

    size of pointer

--- main.cpp
+++ main.cpp
@@ -13,6 +13,20 @@ int garena201708_q01() {
   char* p2 = "Hi";
   char* p3 = (char*)malloc(100);
   char p4[10][10];
+
+  std::cout << sizeof(p1) << std::endl;
+  std::cout << sizeof(p2) << std::endl;
+  std::cout << sizeof(p3) << std::endl;
+  std::cout << sizeof(p4) << std::endl;
+
+  /*
+  #expected output
+  6
+  8
+  8
+  100
+  */
+
 }
 
 int helloWorld() {
